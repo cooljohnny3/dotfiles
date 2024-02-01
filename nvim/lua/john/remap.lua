706461be5887b9ae4a828bc1d5f5fe25b8dc0fa8
John Brehm <cooljohnny3@gmail.com>
2024-02-01 10:02:53 -0800

    nvim: add shortcuts to navigate windows

--- nvim/lua/john/remap.lua
+++ nvim/lua/john/remap.lua
@@ -9,3 +9,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- center screen after jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- 
+vim.keymap.set("n", "<C-h>", "<C-W><C-H>")
+vim.keymap.set("n", "<C-j>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-k>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-l>", "<C-W><C-L>")

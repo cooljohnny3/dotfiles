88d5c0af93baa87ed75d9f99f0c00e85e54b2132
John Brehm <cooljohnny3@gmail.com>
2024-02-16 11:02:09 -0800

    nvim: fix prettier config

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,6 +1,10 @@
 require("conform").setup({
   formatters_by_ft = {
-    javascript = { { "prettier" } },
+    ["javascript"] = { "prettier" },
+    ["javascriptreact"] = { "prettier" },
+    ["typescript"] = { "prettier" },
+    ["typescriptreact"] = { "prettier" },
+    ["vue"] = { "prettier" },
   },
   format_on_save = {
     -- These options will be passed to conform.format()

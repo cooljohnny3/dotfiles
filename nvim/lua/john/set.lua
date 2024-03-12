cda97e6b50656415417dfed3031a70a6a57e8cda
John Brehm <cooljohnny3@gmail.com>
2024-03-12 13:06:24 -0700

    nvim: Add spellcheck autocmd

--- nvim/lua/john/set.lua
+++ nvim/lua/john/set.lua
@@ -52,3 +52,14 @@ vim.o.termguicolors = true
 
 -- Don't show the mode, since it's already in status line
 vim.opt.showmode = false
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("wrap_spell", { clear = true }),
+
+    pattern = { "gitcommit", "markdown" },
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+    end,
+})

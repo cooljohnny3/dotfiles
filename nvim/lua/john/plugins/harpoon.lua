5afd5b88ae8ed807091698350ac617efe4eccb0e
John Brehm <cooljohnny3@gmail.com>
2024-03-12 17:18:28 -0700

    nvim: Add 5-9 shortcuts for Harpoon

--- nvim/lua/john/plugins/harpoon.lua
+++ nvim/lua/john/plugins/harpoon.lua
@@ -16,6 +16,11 @@ return {
         vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "[H]arpoon nav to [2]" })
         vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "[H]arpoon nav to [3]" })
         vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "[H]arpoon nav to [4]" })
+        vim.keymap.set("n", "<leader>h5", function() harpoon:list():select(5) end, { desc = "[H]arpoon nav to [5]" })
+        vim.keymap.set("n", "<leader>h6", function() harpoon:list():select(6) end, { desc = "[H]arpoon nav to [6]" })
+        vim.keymap.set("n", "<leader>h7", function() harpoon:list():select(7) end, { desc = "[H]arpoon nav to [7]" })
+        vim.keymap.set("n", "<leader>h8", function() harpoon:list():select(8) end, { desc = "[H]arpoon nav to [8]" })
+        vim.keymap.set("n", "<leader>h9", function() harpoon:list():select(9) end, { desc = "[H]arpoon nav to [9]" })
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "[H]arpoon nav to [P]rev" })

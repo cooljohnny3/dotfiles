3b2b8f1241d5d0ebf72840ac01f72175146c4114
John Brehm <cooljohnny3@gmail.com>
2024-03-21 16:11:23 -0700

    nvim: Add more gitsigns keybinds

--- nvim/lua/john/plugins/gitsigns.lua
+++ nvim/lua/john/plugins/gitsigns.lua
@@ -20,17 +20,14 @@ return {
 
             -- Actions
             -- normal mode
-            map('n', '<leader>gb', function()
-                gs.blame_line { full = false }
-            end, { desc = '[G]it [B]lame line' })
-            map('n', '<leader>gd', gs.diffthis, { desc = '[G]it [d]iff against index' })
-            map('n', '<leader>gD', function()
-                gs.diffthis '~'
-            end, { desc = '[G]it [D]iff against last commit' })
-
-            -- Toggles
-            map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = '[G]it [T]oggle [B]lame line' })
-            map('n', '<leader>gtd', gs.toggle_deleted, { desc = '[G]it [T]oggle show [D]eleted' })
+            map('n', ']h', function() gs.next_hunk({ navigation_message = false }) end, { desc = 'Next Hunk' })
+            map('n', '[h', function() gs.prev_hunk({ navigation_message = false }) end, { desc = 'Prev Hunk' })
+            map('n', '<leader>gs', function() gs.stage_hunk() end, { desc = '[G]it [S]tage Hunk' })
+            map('n', '<leader>gr', function() gs.reset_hunk() end, { desc = '[G]it [R]eset Hunk' })
+            map('n', '<leader>gu', function() gs.undo_stage_hunk() end, { desc = '[G]it [U]ndo Stage Hunk' })
+            map('n', '<leader>gb', function() gs.blame_line({ full = false }) end, { desc = '[G]it [B]lame line' })
+            map('n', '<leader>gd', function() gs.diffthis() end, { desc = '[G]it [d]iff against index' })
+            map('n', '<leader>gD', function() gs.diffthis('~') end, { desc = '[G]it [D]iff against last commit' })
         end,
     },
 }
--- nvim/lua/john/plugins/init.lua
+++ nvim/lua/john/plugins/init.lua
@@ -57,7 +57,7 @@ return {
     {
         'tpope/vim-fugitive',
         config = function()
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = '[G]it [S]tatus' })
+            vim.keymap.set("n", "<leader>gS", vim.cmd.Git, { desc = '[G]it [S]tatus' })
         end
     },
     -- {

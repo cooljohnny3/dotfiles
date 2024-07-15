3bc0f54d56b19c98b6476bb7b6870790a13d0d52
John Brehm <cooljohnny3@gmail.com>
2024-07-15 09:57:07 -0700

    nvim: Update which-key config for v3 and add undo tree item

--- nvim/lua/john/plugins/which-key.lua
+++ nvim/lua/john/plugins/which-key.lua
@@ -1,16 +1,18 @@
 return {
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function()
-        require('which-key').register {
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-            ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-            ['<leader>gt'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-            ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-            ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-            ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = 'VeryLazy',
+    opts = {
+        spec = {
+            { "<leader>c",  group = "[C]ode" },
+            { "<leader>d",  group = "[D]ocument" },
+            { "<leader>g",  group = "[G]it" },
+            { "<leader>gt", group = "[T]oggle" },
+            { "<leader>h",  group = "[H]arpoon" },
+            { "<leader>r",  group = "[R]ename" },
+            { "<leader>s",  group = "[S]earch" },
+            { "<leader>u",  group = "[U]ndo tree" },
+            { "<leader>w",  group = "[W]orkspace" },
         }
-    end
+    },
 }

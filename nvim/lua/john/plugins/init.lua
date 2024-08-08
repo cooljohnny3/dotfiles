cdca4a040f7e863f85304105f84dcdd2b5cb9f9a
John Brehm <cooljohnny3@gmail.com>
2024-08-08 13:46:32 -0700

    nvim: Set conform to use gofmt for go files

--- nvim/lua/john/plugins/init.lua
+++ nvim/lua/john/plugins/init.lua
@@ -22,6 +22,7 @@ return {
                 ["typescript"] = { "prettier" },
                 ["typescriptreact"] = { "prettier" },
                 ["vue"] = { "prettier" },
+                ["go"] = { "gofmt" },
             }
         }
     },

8eb87a9723ac90a2599699035b61763c5c9dbc67
John Brehm <cooljohnny3@gmail.com>
2025-04-02 16:39:05 -0700

    nvim: Add shortcut keys for todo-comments

--- nvim/lua/john/plugins/init.lua
+++ nvim/lua/john/plugins/init.lua
@@ -30,7 +30,12 @@ return {
         'folke/todo-comments.nvim',
         event = 'VimEnter',
         dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = { signs = false }
+        opts = { signs = false },
+        keys = {
+            { "]t",         function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+            { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+            { "<leader>st", ":TodoTelescope<cr>",                                desc = "[S]earch [T]odos" },
+        }
     },
     {
         'echasnovski/mini.comment',
